4304e0067a64338e489bf847a5dfe439f6ee542a
John E. Lahr <johnelahr@gmail.com>
2014-04-26 15:40:40 -0700

    User Signup Completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SampleApp::Application.routes.draw do
   match 'about', to: 'static_pages#about', via: 'get'
   match 'contact', to: 'static_pages#contact', via: 'get'
   match 'signup', to: 'users#new', via: 'get'
+  resources :users
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
